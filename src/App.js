dadbad34ca26f1e4591243bd36b08b05e1b64e97
ivenckunas <ignas.venckunas@gmail.com>
2023-02-04 15:56:57 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import "./styles/reset.css";
+
+function App() {
+	return (
+		<div className="App container">
+			<h1>remodal</h1>
+		</div>
+	);
+}
+
+export default App;
